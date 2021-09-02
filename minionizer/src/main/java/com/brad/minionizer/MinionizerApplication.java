05fccc4c6fefcf44443ccf3178d91294590bea98
Bradley Weiner <bradleyweiner@Bradleys-MacBook-Pro.local>
2021-09-02 16:47:03 -0500

    init

---
+++ minionizer/src/main/java/com/brad/minionizer/MinionizerApplication.java
@@ -0,0 +1,13 @@
+package com.brad.minionizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinionizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinionizerApplication.class, args);
+	}
+
+}
---
+++ minionizer/src/test/java/com/brad/minionizer/MinionizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brad.minionizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinionizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
